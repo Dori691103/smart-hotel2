8c84f205531afafff0298e680877a48a4e4ebfc8
Dori691103 <miskamoiseev123@gmail.com>
2025-12-17 04:28:10 +0400

    Create BookingRequest.java

---
+++ BookingRequest.java
@@ -0,0 +1,14 @@
+package com.travel.booking.dto;
+
+import java.time.LocalDate;
+
+public class BookingRequest {
+    private String city;
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+    private Integer adults;
+    private Integer children;
+    private String preferredHotel;
+    
+    // геттеры и сеттеры
+}
